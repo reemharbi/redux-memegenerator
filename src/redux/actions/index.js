ffd6b37ac4380c7344fb9ec3ec68635629958d70
reemharbi <reemharbi.3@gmail.com>
2020-02-25 17:46:53 +0300

    Setting up store and middleware for async actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ export const RECEIVE_MEMES = 'RECEIVE_MEMES';
 function receiveMemes(json) {
     // find memes in json data
     const { memes } = json.data;
-    // return an action
+    // return an action and memes object
     return {
         type: RECEIVE_MEMES,
         memes
@@ -16,12 +16,19 @@ function fetchMemesJson(){
     .then(response => response.json())
 }
 
-
+// A function that returns an inner function
 export function fetchMemes(){
-    // dispatch allows us to return an inner function
+    // dispatch allows us to dispatch to recieve JSON
+    // at any moment and handle the aynchrenous behaviour 
+    // of the api
     return function(dispatch){
         // return a call to the fetchMemes json
         return fetchMemesJson()
         .then(json => dispatch(receiveMemes(json)))
     }
 }
+
+
+// NOTE: dispatch is a function of the Redux store. 
+//      You call store.dispatch to dispatch an action. 
+//      This is the only way to trigger a state change.
