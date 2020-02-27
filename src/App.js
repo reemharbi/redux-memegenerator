fc8e52a30245331530144bc6aa0577a4d20270f7
reemharbi <reemharbi.3@gmail.com>
2020-02-27 16:27:22 +0300

    Add text form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
-import '../src/styles/index.css';
+import '../src/styles/index.scss';
 import { connect } from 'react-redux';
-
+// import BeatLoader from 'react-spinners/BeatLoader';
 import MemeItem from '../src/components/meme-item/memeItem';
+import { Form } from 'react-bootstrap';
 
 class App extends Component {
     constructor() {
         super();
+
         this.state = {
-            memeLimit: 10
+            memeLimit: 10,
+            loading: true,
+            text0: '',
+            text1: ''
         };
     }
+
     render() {
+      // console.log(this.state);
         return (
             <div className='App'>
-                <h1>Welcome to the MemeGenerator!</h1>
+                <h1>Welcome To The MemeGenerator!</h1>
+                <h4>Write Some Text</h4>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Top</Form.Label>
+                        <Form.Control
+                            type='text'
+                            onChange={(event) =>
+                                this.setState({ text0: event.target.value })
+                            }
+                        ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Bottom</Form.Label>
+                        <Form.Control
+                            type='text'
+                            onChange={(event) =>
+                                this.setState({ text1: event.target.value })
+                            }
+                        ></Form.Control>
+                    </Form.Group>
+                </Form>
                 {this.props.memes
                     .slice(0, this.state.memeLimit)
                     .map((meme, index) => {
